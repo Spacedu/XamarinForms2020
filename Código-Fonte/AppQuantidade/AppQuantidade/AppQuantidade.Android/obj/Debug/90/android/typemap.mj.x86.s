a58ec6691b0ffa293bb9891096015e50ca170ce1
Elias <contato@spacedu.com.br>
2020-02-28 16:57:37 -0300

    Projeto do Cap 03

---
+++ Código-Fonte/AppQuantidade/AppQuantidade/AppQuantidade.Android/obj/Debug/90/android/typemap.mj.x86.s
@@ -0,0 +1,25 @@
+	/* Data Hash: AD075AD7BE9F8528 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1428
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 374137
+	.include	"typemap.mj.inc"
---
+++ Código-Fonte/AppQuantidade/AppQuantidade/AppQuantidade/MainPage.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace AppQuantidade
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        int quantidade = 1;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Aumentar(object sender, EventArgs e)
+        {
+            quantidade++;
+            LblQuantidade.Text = quantidade.ToString();
+        }
+
+        private void Diminuir(object sender, EventArgs e)
+        {
+            if(quantidade != 0)
+            {
+                quantidade--;
+                LblQuantidade.Text = quantidade.ToString();
+            }            
+        }
+    }
+}
