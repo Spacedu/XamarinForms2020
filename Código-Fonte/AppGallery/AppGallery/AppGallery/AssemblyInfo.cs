f7ea362a457f77fa7fae8b79d99ee74717a6fabd
Elias <contato@spacedu.com.br>
2020-04-23 13:19:56 -0300

    -

--- Código-Fonte/AppGallery/AppGallery/AppGallery/AssemblyInfo.cs
+++
@@ -1,8 +0,0 @@
-using Xamarin.Forms;
-using Xamarin.Forms.Xaml;
-
-[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
-[assembly: ExportFont("gotham.ttf", Alias = "Gotham")]
-[assembly: ExportFont("gotham-black.ttf", Alias = "GothamBlack")]
-[assembly: ExportFont("gotham-light.ttf", Alias = "GothamLight")]
-[assembly: ExportFont("font-awesome.otf", Alias = "FontAwesome")]
